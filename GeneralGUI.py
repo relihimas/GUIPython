bea86ddc806f3bff9629402bce5358f9d2d64990
Rachid Elihimas <60620695+relihimas@users.noreply.github.com>
2021-04-26 17:42:04 -0300

    Update GeneralGUI.py
    adição de novos campos

--- GeneralGUI.py
+++ GeneralGUI.py
@@ -4,15 +4,70 @@ from PyQt5.QtWidgets import *
 import sys
 from envioSQL import Acesso
 
-class Window(QWindow):
+
+class Window(QWidget):
     def __init__(self):
-        QWindow.__init__(self)
-        self.setTitle("GUI de Teste")
-        self.resize(400, 300)
+        QWidget.__init__(self)
+        self.setWindowTitle("Envio de Dados para o SQL")           #título
+        # self.setWindowIcon()                            ícone
+        # self.setFixedHeight(500)                        altura
+        # self.setFixedWidth(400)                         largura
+
+        layout = QGridLayout()
+        self.setLayout(layout)
+
+        nome = QLabel("Nome: ")
+        layout.addWidget(nome, 0, 0)
+
+        endereco = QLabel("Endereço: ")
+        layout.addWidget(endereco, 1, 0)
+
+        cidade = QLabel("Cidade: ")
+        layout.addWidget(cidade, 2, 0)
+
+        companhia = QLabel("Companhia: ")
+        layout.addWidget(companhia, 3, 0)
+
+        data = QLabel("Data Cadastro: ")
+        layout.addWidget(data, 4, 0)
+
+        self.linenome = QLineEdit()
+        # self.linenome.returnPressed.connect(self.return_pressed)
+        layout.addWidget(self.linenome, 0, 1)
+
+        self.lineend = QLineEdit()
+        # self.lineend.returnPressed.connect(self.return_pressed)
+        layout.addWidget(self.lineend, 1, 1)
+
+        self.linecidade = QLineEdit()
+        # self.linecidade.returnPressed.connect(self.return_pressed)
+        layout.addWidget(self.linecidade, 2, 1)
+
+        self.linecomp = QLineEdit()
+        # self.linecomp.returnPressed.connect(self.return_pressed)
+        layout.addWidget(self.linecomp, 3, 1)
+
+        self.linedata = QLineEdit()
+        # self.linedata.returnPressed.connect(self.return_pressed)
+        layout.addWidget(self.linedata, 4, 1)
+
+        button = QPushButton("Clique para enviar")
+        button.clicked.connect(self.on_button_clicked)
+        layout.addWidget(button, 10, 1)
 
 
-envio = Acesso()
-envio.envioclientetipo()
+    def on_button_clicked(self):
+        print("The button was pressed!")
+        # Nome, Endereço, Cidade, Companhia, DTCadastro
+        self.nome = self.linenome.text()
+        self.endereco = self.lineend.text()
+        self.cidade = self.linecidade.text()
+        self.companhia = self.linecomp.text()
+        self.data = self.linedata.text()
+        envio = Acesso()
+        envio.envioclientetipo(self.nome, self.endereco, self.cidade, self.companhia, self.data)
+        print("Envio com sucesso!!")
+
 
 app = QApplication(sys.argv)
 screen = Window()
