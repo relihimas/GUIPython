d66b4f907fceff6c4a55e3007e427bce4f657186
Rachid Elihimas <60620695+relihimas@users.noreply.github.com>
2021-05-06 14:44:06 -0300

    Update envioSQL.py

--- envioSQL.py
+++ envioSQL.py
@@ -3,21 +3,39 @@ import pandas as pd
 
 class Acesso:
     def __init__(self):
-        self.server = '#####'         #Database server
-        self.database = '#####'       #Database
+        self.server = '#####'
+        self.database = 'Teste'
         self.username = ''
         self.password = ''
-        self.arq = pd.read_excel(
-            "PASS HERE YOUR SOURCE")
         self.cnxn = pyodbc.connect('DRIVER={SQL Server};SERVER='+self.server+';DATABASE='+self.database+';UID='+self.username+';PWD='+self.password)
         self.cursor = self.cnxn.cursor()
 
-    def envioclientetipo(self):
-        for index, row in self.arq.iterrows():
-            self.cursor.execute("INSERT INTO clienteTipo (Nome, Endereço, Cidade, Companhia, DTCadastro) values(?, ?, ? ,? ,?)",
-                                    row.Nome, row.Endereço, row.Cidade, row.Companhia, row.Data)
+    def enviousuario(self, nome, endereco, cidade, companhia, data):
+        self.cursor.execute("INSERT INTO Usuario (Nome, Endereço, Cidade, Companhia, DTCadastro) values(?, ?, ? ,? ,?)",
+                                    nome, endereco, cidade, companhia, data)
         self.cnxn.commit()
         self.cursor.close()
 
-envio = Acesso()
-envio.envioclientetipo()
+    def procedure_cadastrousuario(self, nome, endereco, cidade, companhia, data):
+        prcdcadastrousuario = 'EXEC prcd_cadastrousuario @varNome = ?, @varEndereco = ?, @varCidade = ?, @varCompanhia = ?, @varData = ?;'
+        params = (nome, endereco, cidade, companhia, data)
+        self.cursor.execute(prcdcadastrousuario,params)
+        self.cnxn.commit()
+        self.cursor.close()
+
+    # def checklogin(self, usuario, senha):
+    #     return True
+
+
+
+# envio = Acesso()
+# envio.envioclientetipo()
+
+
+# def envioclientetipo(self, nome, endereco, cidade, companhia, data):
+#     for index, row in self.arq.iterrows():
+#         self.cursor.execute(
+#             "INSERT INTO clienteTipo (Nome, Endereço, Cidade, Companhia, DTCadastro) values(?, ?, ? ,? ,?)",
+#             row.Nome, row.Endereço, row.Cidade, row.Companhia, row.Data)
+#     self.cnxn.commit()
+#     self.cursor.close()
