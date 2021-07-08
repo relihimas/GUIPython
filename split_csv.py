f5d1d3eab7b0c90fabed7d8d566e40d01229d5c7
Rachid Elihimas <60620695+relihimas@users.noreply.github.com>
2021-07-08 14:48:45 -0300

    Update split_csv.py

--- split_csv.py
+++ split_csv.py
@@ -1,51 +1,55 @@
 import pandas as pd
 
-arqcsv = "CAMINHO DO ARQUIVO"
+source = r"CAMINHO"
+local = r"CAMINHO DO ARQUIVO"
 
+def splitcsv(file):
+    '''abrindo o arquivo'''
+    arquivo = pd.read_excel(file)
 
-def opencsv(file):
-  arquivo = pd.read_csv(file)
-  arquivo['index'] = arquivo.index
-  linha = arquivo.iloc[-1::]
-  return list(linha['index'])
+    '''criando a coluna de índice'''
+    arquivo['index'] = arquivo.index
 
-arquivo = pd.read_csv(arqcsv)
-arquivo['index'] = arquivo.index
+    '''criando uma lista com os nomes das colunas originais para dividir nos arquivos divididos'''
+    colunas = list(arquivo.keys())
 
-primeira_linha = arquivo.iloc[0:1]
-prima_linha = int(primeira_linha['index'])
+    '''quantidade de linhas para cada arquivo'''
+    qtd_max = 98000
 
-linha = arquivo.iloc[-1::]
-ult_linha = int(linha['index'])
+    '''índice da primeira linha do arquivo'''
+    primeira_linha = arquivo.iloc[0:1]
+    prima_linha = int(primeira_linha['index'])
 
-print(prima_linha)
-print(ult_linha)
+    '''índice da última linha do arquivo'''
+    ultima_linha = arquivo.iloc[-1::]
+    ult_linha = int(ultima_linha['index'])
 
-divisao = int(round(ult_linha / 98000,0))
-print(divisao)
+    '''outros dados'''
+    lin_inicio = prima_linha
+    nome = "TESTE"
 
-qtd_max = 98000
-lin_inicio = prima_linha
-nome = "TESTE"
+    '''quantidade de arquivos a serem divididos'''
+    divisao = int(round(ult_linha / 98000, 0))
 
-for i in range(1, divisao+1):
-  max_linha = (i * qtd_max)
-  sobra_linha = ult_linha - (i * qtd_max)
-  
-  #print(f"{i} | {lin_inicio} - {max_linha} | {qtd_max * (i-1) + 1} - {max_linha} | {sobra_linha}")
+    '''check divisao'''
+    if divisao == 0:
+        divisao = 1
+    else:
+        divisao
 
-  if i == 1:
-    arch = arquivo.iloc[lin_inicio:max_linha+1]
-  else:
-    arch = arquivo.iloc[(qtd_max * (i-1) + 1):max_linha+1]
+    '''loop para criação dos arquivos'''
+    for i in range(1, divisao + 1):
+        max_linha = (i * qtd_max)
 
-  arch.to_csv(f'{nome}_{i}.csv')
+        if i == 1:
+            arch = arquivo.iloc[lin_inicio:max_linha + 1]
+        else:
+            arch = arquivo.iloc[(qtd_max * (i - 1) + 1):max_linha + 1]
 
-  lin_inicio += 1
+        arch.to_excel(f'{source}'+f'\{nome}_{i}.xlsx', encoding='utf-8', columns=colunas, index_label='index')
 
-print("fim")
+        lin_inicio += 1
 
-#with open(f'{nome}_{i}.csv', mode='a', newline='') as csvfile:
-#writer = csv.writer(csvfile)
-#for r in row:
-#writer.writerow(arch)
+    print("fim")
+
+splitcsv(local)
