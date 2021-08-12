d42baf9b4b8bfe2edaa5407d0352e92000785a19
Rachid Elihimas <60620695+relihimas@users.noreply.github.com>
2021-08-11 22:16:35 -0300

    Update speechRecon.py

--- speechRecon.py
+++ speechRecon.py
@@ -4,6 +4,7 @@
 import speech_recognition as sr #speech to text
 import time
 import pyttsx3
+from reportlab.pdfgen import canvas
 
 r = sr.Recognizer()
 
@@ -14,6 +15,24 @@ with mic as source:
     order = r.recognize_google(audio, language='pt-br')
 
 print(order)
+
+def generatePDF(texto):
+    try:
+        nome_pdf = "Teste"
+        pdf = canvas.Canvas(f'{nome_pdf}.pdf')
+        x = 720
+        pdf.setTitle(nome_pdf)
+        pdf.drawString(247, x, texto)
+        pdf.setFont('Helvetica-Oblique',14)
+        pdf.save()
+        print("PDF gerado com sucesso")
+    except:
+        print("Erro ao gerar o pdf")
+
+generatePDF(order)
+
+
+
 
 #### TEXT TO SPEECH ####
 engine = pyttsx3.init()
